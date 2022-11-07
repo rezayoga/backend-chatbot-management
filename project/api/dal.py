649e6bc3b6ec32812d294f09b5594c2fc0b10df7
Reza Yogaswara <reza.yoga@gmail.com>
2022-11-07 16:16:10 +0700

    Updating

--- project/api/dal.py
+++ project/api/dal.py
@@ -116,14 +116,6 @@ class Active_Template_DAL:
 		return active_template
 
 	@classmethod
-	async def get_active_template(cls, session: AsyncSession) -> Union[bool, Active_Template]:
-		at = await session.execute(select(Active_Template))
-		active_template = at.scalars().all()
-		if not active_template:
-			return False
-		return active_template
-
-	@classmethod
 	async def get_active_template_by_id(cls, active_template_id: str, session: AsyncSession) -> Union[
 		bool, Active_Template]:
 		at = await session.execute(
